a766360d8dda74844b5ecb2aeb75eb4fa440a092
ibostan <irina.bostan0@gmail.com>
2025-09-10 10:40:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Testing Project with Copilot
+
+This is a simple project to test GitHub Copilot's capabilities in generating code snippets 
+and completing code based on comments and existing code.
+
+## Features
+- Spring boot application that represents a school catalog
+- Database schema in schema.sql
+- Database migration using flyway
+- In-memory database for testing
+- API contract in openapi.yaml file
+- All pojos are generated from the openapi.yaml file
+- Pojos are mapped to DB entities using mapstruct library
+- RESTful API endpoints
+- Basic CRUD operations
+- Spring boot tests
+- Unit tests
+- Dockerfile for containerization
---
+++ create-project-folder-structure.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+PROJECT_NAME="$0"
+BASE="src/main/java/com/ibostan/$PROJECT_NAME"
+BASE_TEST="src/test/java/com/ibostan/$PROJECT_NAME"
+
+project_folders=(
+   "setup/scripts"
+   "monitoring/splunk/alerts"
+   "monitoring/splunk/dashboards"
+   "$BASE"
+   "$BASE_TEST"
+)
+
+app_folders=(
+   "$BASE/application/configuration"
+   "$BASE/application/properties"
+   "$BASE/model/database/entities"
+   "$BASE/model/database/repositories"
+   "$BASE/model/mapper"
+   "$BASE/model/enums"
+   "$BASE/model/annotations"
+   "$BASE/model/exceptions"
+   "$BASE/model/exceptions/downstreams"
+   "$BASE/domain/service"
+   "$BASE/domain/service/aspects"
+   "$BASE/domain/service/utils"
+   "$BASE/domain/service/downstreams"
+   "$BASE/domain/service/downstreams/interceptors"
+   "$BASE/domain/service/downstreams/errorhandlers"
+   "$BASE/domain/logging"
+   "$BASE/domain/tracing"
+   "$BASE/web/api"
+   "$BASE/web/errorhandling"
+   "$BASE/web/interceptors"
+   "$BASE/web/filters"
+)
+
+db_migrations_folders=(
+   "src/main/resources/db/migrations/common"
+   "src/main/resources/db/migrations/environments"
+   "src/main/resources/db/migrations/testdata"
+)
+
+create_folders() {
+   for folder in "$@"; do
+      mkdir -p "$folder"
+   done
+}
+
+create_folders "${project_folders[@]}"
+create_folders "${app_folders[@]}"
+create_folders "${db_migrations_folders[@]}"
+
+echo "Folder structure created."
